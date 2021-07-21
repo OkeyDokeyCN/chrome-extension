a7aec4ef0141c8711eff37c79f38353e8cc2ed45
OkeyDokeyCN <64335475+OkeyDokeyCN@users.noreply.github.com>
2021-07-20 21:16:24 -0400

    injected foreground script to tab

---
+++ foreground.js
@@ -0,0 +1,7 @@
+//fun exercise
+// document.querySelector(
+//   "body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img"
+// ).style.transitionDuration = "2.0s";
+// document.querySelector(
+//   "body > div.L3eUgb > div.o3j99.LLD4me.yr19Zb.LS8OJ > div > img"
+// ).style.transform = "rotate(1000deg)";
